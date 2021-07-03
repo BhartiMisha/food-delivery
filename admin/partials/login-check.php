f642d4966fb826c35e6a61b53cda3937454e9c84
rohanbhard <1929036@kiit.ac.in>
2021-07-03 20:18:10 +0530

    Update login-check.php

--- admin/partials/login-check.php
+++ admin/partials/login-check.php
@@ -1,6 +1,14 @@
 <?php 
-//authorization- access control
-//check whether the user is logged in or not
 
+    //AUthorization - Access COntrol
+    //CHeck whether the user is logged in or not
+    if(!isset($_SESSION['user'])) //IF user session is not set
+    {
+        //User is not logged in
+        //REdirect to login page with message
+        $_SESSION['no-login-message'] = "<div class='error text-center'>Please login to access Admin Panel.</div>";
+        //REdirect to Login Page
+        header('location:'.SITEURL.'admin/login.php');
+    }
 
 ?>

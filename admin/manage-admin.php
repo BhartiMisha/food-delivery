473bb09cbef512e920443db0c87abd43057bad62
rohanbhard <1929036@kiit.ac.in>
2021-07-03 20:24:36 +0530

    Update manage-admin.php

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,10 +4,49 @@
         <div class="main admin-main">
         <div class="wrapper">
             <h1 >Admin page</h1>
-            <br/> <br/> 
+            <br/> <br/> <?php 
+                    if(isset($_SESSION['add']))
+                    {
+                        echo $_SESSION['add']; //Displaying Session Message
+                        unset($_SESSION['add']); //REmoving Session Message
+                    }
+
+                    if(isset($_SESSION['delete']))
+                    {
+                        echo $_SESSION['delete'];
+                        unset($_SESSION['delete']);
+                    }
+                    
+                    if(isset($_SESSION['update']))
+                    {
+                        echo $_SESSION['update'];
+                        unset($_SESSION['update']);
+                    }
+
+                    if(isset($_SESSION['user-not-found']))
+                    {
+                        echo $_SESSION['user-not-found'];
+                        unset($_SESSION['user-not-found']);
+                    }
+
+                    if(isset($_SESSION['pwd-not-match']))
+                    {
+                        echo $_SESSION['pwd-not-match'];
+                        unset($_SESSION['pwd-not-match']);
+                    }
+
+                    if(isset($_SESSION['change-pwd']))
+                    {
+                        echo $_SESSION['change-pwd'];
+                        unset($_SESSION['change-pwd']);
+                    }
+
+                ?>
+                <br><br><br>
+            
             <a href="add-admin.php" class='btn-primary'>Add Admin</a>
             
-            <br/><br/>
+            <br /><br /><br />
             <table class=" tbl-full">
                 <tr>
                     <th>S.no</th>
@@ -41,8 +80,9 @@
                             <td> <?php echo $full_name; ?></td>
                             <td><?php echo $username; ?></td>
                             <td>
-                            <a href="update-admin.php" class='btn-secondary'>Update Admin</a>
-                            <a href="delete-admin.php" class='btn-danger'>Delete Admin</a>
+                            <a href="<?php echo SITEURL; ?>admin/update-password.php?id=<?php echo $id; ?>" class="btn-primary">Change Password</a>
+                            <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-secondary">Update Admin</a>
+                            <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete Admin</a>
                             </td>
                             </tr>
 
